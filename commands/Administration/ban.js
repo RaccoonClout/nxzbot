93bf52009a53ab4694d6b3bddca24ad6c45dcd59
naz <42706009+n4zzu@users.noreply.github.com>
2021-08-14 19:57:23 +0100

    added ban cmd

---
+++ commands/Administration/ban.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+module.exports = {
+    name: "ban",
+    category: "Administration",
+    aliases: ["bab", "1"],
+    cooldown: 1,
+    usage: "ban",
+    description: "Bans a user",
+    run: async (client, message, args, user, text, prefix) => {
+    try{
+      message.channel.send(new MessageEmbed()
+        .setColor(ee.color)
+        .setFooter(ee.footertext, ee.footericon)
+        .setTitle(`test cmd for banning`)
+      );
+    } catch (e) {
+        console.log(String(e.stack).bgRed)
+        return message.channel.send(new MessageEmbed()
+            .setColor(ee.wrongcolor)
+            .setFooter(ee.footertext, ee.footericon)
+            .setTitle(`❌ ERROR | An error occurred`)
+            .setDescription(`\`\`\`${e.stack}\`\`\``)
+        );
+    }
+  }
+}
+
+/** Template by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template */
